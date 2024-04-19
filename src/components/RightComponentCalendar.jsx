d12f6a36eb26411c0a2089c1a60e1aa7d83bc77e
Juanes <juan.ortiz-p@mail.escuelaing.edu.co>
2024-04-19 18:18:00 -0500

    Calendar

--- src/components/RightComponentCalendar.jsx
+++ src/components/RightComponentCalendar.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import './RightComponentCalendar.css';
 const RightComponentCalendar = () => {
   return (
     <div className="right-box">
-      <h2>Título del Cuadro</h2>
+      <h2>Calendar</h2>
       <ul>
         <li>Opción 1</li>
         <li>Opción 2</li>
